c3d05710f16a57f92a6d2663678084fa47483610
edevey27 <edevey27@gmail.com>
2024-10-14 22:04:06 -0600

    updated colors

--- Step1/static/js/logic.js
+++ Step1/static/js/logic.js
@@ -14,15 +14,13 @@ function getColor(depth) {
     if (depth > 90) {
         return "#FF0000"; // Red for depths greater than 90
     } else if (depth > 70) {
-        return "#FF4500"; // Dark Orange for depths greater than 70
+        return "#FFA500"; // Orange for depths greater than 70
     } else if (depth > 50) {
-        return "#FFA500"; // Orange for depths greater than 50
+        return "#FFFF00"; // Yellow for depths greater than 50
     } else if (depth > 30) {
-        return "#FFFF00"; // Yellow for depths greater than 30
-    } else if (depth > 10) {
-        return "#ADFF2F"; // Light Green for depths greater than 10
+        return "#ADFF2F"; // Yellow-Green for depths greater than 30
     } else {
-        return "#00FF00"; // Dark Green for depths less than or equal to 10
+        return "#00FF00"; // Dark Green for depths less than or equal to 30
     }
 }
 
@@ -71,7 +69,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     legend.onAdd = function(){
         let container = L.DomUtil.create("div", "info legend");
         let grades = [-10, 10, 30, 50, 70, 90];
-        let colors = ['#00FF00', '#ADFF2F', '#FFFF00', '#FFA500', '#FF4500', '#FF0000'];
+        let colors = ['#00FF00', '#ADFF2F', '#FFFF00', '#FFA500', '#FF0000'];
         for(let index = 0; index < grades.length; index++) {
            container.innerHTML += `<i style="background: ${colors[index]}"></i> ${grades[index]}+ <br>` 
         }
